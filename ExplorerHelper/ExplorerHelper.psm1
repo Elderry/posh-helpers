640780d5fb9e8bfceb605ae8ac42ba940f7878c0
Ruiyang Li <Elderry@outlook.com>
2020-12-21 23:51:10 +0800

    Added module ExplorerHelper.

---
+++ ExplorerHelper/ExplorerHelper.psm1
@@ -0,0 +1,72 @@
+<#
+.SYNOPSIS
+Flatten all files under directories recursively into current directory.
+#>
+function Flatten-File {
+
+    [CmdletBinding()]
+    param (
+        # Whether to operate recursively.
+        [switch] $WithPrefix,
+        # Whether to overwrite on existing file.
+        [switch] $Force
+    )
+
+    Get-ChildItem -Recurse -File | ForEach-Object {
+
+        if ($_.Directory.FullName -eq $PWD) { Return }
+
+        $dest = if ($WithPrefix) {
+            ($_.Directory.FullName -replace "$PWD\", '' | ForEach-Object { $_ -replace '\\', '_' }) + "_$($_.Name)"
+        } else {
+            "$PWD/$($_.Name)"
+        }
+
+        if (Test-Path -LiteralPath $dest) {
+            if ($Force) { Remove-Item $dest }
+            else {
+                Write-Warning "File [$dest] already exists, skipping..."
+                Return
+            }
+        }
+
+        Move-Item -LiteralPath $_.FullName $dest
+    }
+
+    Get-ChildItem -Directory | ForEach-Object { Trim-Directory $_ }
+}
+Export-ModuleMember -Function Flatten-File
+
+function Trim-Directory($directory) {
+    Get-ChildItem -LiteralPath $directory -Directory | ForEach-Object { Trim-Directory $_ }
+    if ((Get-ChildItem -LiteralPath $directory).Length -eq 0) { Remove-Item -LiteralPath $directory }
+}
+
+<#
+.SYNOPSIS
+Remove common prefix for jpg files.
+#>
+function Remove-Prefix {
+
+    [CmdletBinding()]
+    param ()
+
+    $names = Get-ChildItem -Filter *.jpg | ForEach-Object { $_.Name }
+    if ($names.Length -lt 2) { Return }
+    $bases = $names | ForEach-Object { Split-Path -LeafBase $_ }
+
+    $prefix = $bases[0]
+    ForEach ($name in $bases[1..$bases.Length]) {
+        $range = [Math]::Min($prefix.Length, $name.Length) - 1
+        ForEach ($i in (0..$range)) {
+            if ($prefix[$i] -ne $name[$i]) {
+                $prefix = $prefix.Substring(0, ($i - 1))
+                break
+            }
+        }
+        if (!$prefix) { Return }
+    }
+
+    $names | Rename-Item -NewName { $_ -replace "${prefix}0*", '' }
+}
+Export-ModuleMember -Function Remove-Prefix
